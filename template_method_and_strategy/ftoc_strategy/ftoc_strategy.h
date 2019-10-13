ec6d0ac4e9e988dad983d8f245b104337dd917c6
mli9502 <mli9502@outlook.com>
2019-10-13 18:32:59 -0400

    Test hook again.

--- template_method_and_strategy/ftoc_strategy/ftoc_strategy.h
+++ template_method_and_strategy/ftoc_strategy/ftoc_strategy.h
@@ -4,48 +4,43 @@
 #include <application.h>
 
 class FtocStrategy : public Application {
-private:
-    string m_testFileName;
-    shared_ptr<ifstream> m_fd;
-    bool m_isDone;
+ private:
+  string m_testFileName;
+  shared_ptr<ifstream> m_fd;
+  bool m_isDone;
 
-protected:
-    void init() override {
-        m_fd = make_shared<ifstream>(m_testFileName);
+ protected:
+  void init() override { m_fd = make_shared<ifstream>(m_testFileName); }
+
+  void idle() override {
+    if (!m_fd->is_open()) {
+      m_isDone = true;
+      return;
+    }
+    string line;
+    if (!getline(*m_fd, line)) {
+      m_isDone = true;
+      return;
     }
 
-    void idle() override {
-        if(!m_fd->is_open()) {
-            m_isDone = true;
-            return;
-        }
-        string line;
-        if(!getline(*m_fd, line)) {
-            m_isDone = true;
-            return;
-        }
-
-        try {
-            double fahr = stod(line);
-            double celcius = 5.0 / 9.0 * (fahr - 32.0);
-            cout << "F = " << fahr << ", C = " << celcius << endl;
-        } catch(const invalid_argument& e) {
-            cerr << e.what() << endl;
-        } catch(const out_of_range& e) {
-            cerr << e.what() << endl;
-        }
+    try {
+      double fahr = stod(line);
+      double celcius = 5.0 / 9.0 * (fahr - 32.0);
+      cout << "F = " << fahr << ", C = " << celcius << endl;
+    } catch (const invalid_argument& e) {
+      cerr << e.what() << endl;
+    } catch (const out_of_range& e) {
+      cerr << e.what() << endl;
     }
+  }
 
-    void cleanup() override {
-        cout << "ftoc exit..." << endl;
-    }
+  void cleanup() override { cout << "ftoc exit..." << endl; }
 
-    bool done() override {
-        return m_isDone;
-    }
+  bool done() override { return m_isDone; }
 
-public:
-    FtocStrategy(const string& testFileName) : m_testFileName(testFileName), m_isDone(false) {}
+ public:
+  FtocStrategy(const string& testFileName)
+      : m_testFileName(testFileName), m_isDone(false) {}
 };
 
 #endif
